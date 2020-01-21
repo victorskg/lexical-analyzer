e1e488dba41da1931e2b01de0b6b098f9db5854a
victorskg <victorskg@outlook.com>
2020-01-21 17:26:40 -0300

    Refatorações para melhoria do código.

--- src/main/java/com/victorskg/model/TokenType.java
+++ src/main/java/com/victorskg/model/TokenType.java
@@ -1,46 +1,61 @@
 package com.victorskg.model;
 
+import java.util.regex.Matcher;
+
+import static java.util.regex.Pattern.DOTALL;
+import static java.util.regex.Pattern.compile;
+
 public enum TokenType {
 
-    BlockComment,
-    LineComment,
-    WhiteSpace,
-    Tab,
-    NewLine,
-    CloseBrace,
-    OpenBrace,
-    OpeningCurlyBrace,
-    ClosingCurlyBrace,
-    DoubleConstant,
-    IntConstant,
-    Plus,
-    Minus,
-    Multiply,
-    Divide,
-    Point,
-    EqualEqual,
-    Equal,
-    ExclamationEqual,
-    Greater,
-    Less,
-    Static,
-    Public,
-    Private,
-    Int,
-    Double,
-    Void,
-    False,
-    True,
-    Null,
-    Return,
-    New,
-    Class,
-    If,
-    While,
-    Else,
-    Semicolon,
-    Comma,
-    Identifier;
+    BlockComment("(/\\*.*?\\*/).*"),
+    LineComment("(//(.*?)[\r$]?\n).*"),
+    WhiteSpace("( ).*"),
+    OpenBrace("(\\().*"),
+    CloseBrace("(\\)).*"),
+    Semicolon("(;).*"),
+    Comma("(,).*"),
+    OpeningCurlyBrace("(\\{).*"),
+    ClosingCurlyBrace("(\\}).*"),
+    DoubleConstant("\\b(\\d{1,9}\\.\\d{1,32})\\b.*"),
+    IntConstant("\\b(\\d{1,9})\\b.*"),
+    Void("\\b(void)\\b.*"),
+    Int("\\b(int)\\b.*"),
+    Double("\\b(int|double)\\b.*"),
+    Tab("(\\t).*"),
+    NewLine("(\\n).*"),
+    Public("\\b(public)\\b.*"),
+    Private("\\b(private)\\b.*"),
+    False("\\b(false)\\b.*"),
+    True("\\b(true)\\b.*"),
+    Null("\\b(null)\\b.*"),
+    Return("\\b(return)\\b.*"),
+    New("\\b(new)\\b.*"),
+    Class("\\b(class)\\b.*"),
+    If("\\b(if)\\b.*"),
+    Else("\\b(else)\\b.*"),
+    While("\\b(while)\\b.*"),
+    Static("\\b(static)\\b.*"),
+    Point("(\\.).*"),
+    Plus("(\\+{1}).*"),
+    Minus("(\\-{1}).*"),
+    Multiply("(\\*).*"),
+    Divide("(/).*"),
+    EqualEqual("(==).*"),
+    Equal("(=).*"),
+    ExclamationEqual("(\\!=).*"),
+    Greater("(>).*"),
+    Less("(<).*"),
+    Identifier("\\b([a-zA-Z]{1}[0-9a-zA-Z_]{0,31})\\b.*");
+
+    private String regex;
+
+    TokenType(String regex) {
+        this.regex = regex;
+    }
+
+    public Matcher getMatcher(int fromIndex, String source) {
+        return compile(".{" + fromIndex + "}" + this.regex, DOTALL).matcher(source);
+    }
 
     public boolean isAuxiliary() {
         return this == BlockComment || this == LineComment || this == NewLine || this == Tab || this == WhiteSpace;
--- src/main/java/com/victorskg/service/LexicalAnalyzerService.java
+++ src/main/java/com/victorskg/service/LexicalAnalyzerService.java
@@ -14,6 +14,7 @@ import static com.victorskg.model.TokenType.Double;
 import static com.victorskg.model.TokenType.Void;
 import static com.victorskg.model.TokenType.*;
 import static java.lang.String.format;
+import static java.util.List.of;
 import static java.util.Objects.nonNull;
 import static java.util.regex.Pattern.DOTALL;
 import static java.util.regex.Pattern.compile;
@@ -21,10 +22,7 @@ import static java.util.regex.Pattern.compile;
 @Service
 public class LexicalAnalyzerService {
 
-    private Map<TokenType, String> regEx;
-
     public LexicalAnalyzerService() {
-        regEx = new TreeMap<>();
     }
 
     public List<Token> analyze(String source) throws AnalyzerException {
@@ -40,73 +38,26 @@ public class LexicalAnalyzerService {
             }
         } while (nonNull(token) && !Objects.equals(position, source.length()));
 
-        if (position != source.length()) {
-            throw new AnalyzerException(format("Errro léxico na posição %d!", position), position);
-        }
+        if (!Objects.equals(position, source.length()))
+            throw new AnalyzerException(format("Erro léxico na posição %d!", position), position);
 
         return tokens;
     }
 
-    public List<Token> getFilteredTokens(List<Token> tokens) {
-        return tokens.stream().filter(t -> !t.getTokenType().isAuxiliary()).collect(Collectors.toList());
-    }
-
     private Token separateToken(String source, int fromIndex) {
-        if (fromIndex < 0 || fromIndex >= source.length()) {
+        if (fromIndex < 0 || fromIndex >= source.length())
             throw new IllegalArgumentException("Index fora de alcance!");
-        }
-        for (var tokenType : TokenType.values()) {
-            var p = compile(".{" + fromIndex + "}" + regEx.get(tokenType), DOTALL);
-            var m = p.matcher(source);
-            if (m.matches()) {
-                var lexema = m.group(1);
+
+        /*for (var tokenType : TokenType.values()) {
+            var matcher = tokenType.getMatcher(fromIndex, source);
+            if (matcher.matches()) {
+                var lexema = matcher.group(1);
                 return new Token(fromIndex, fromIndex + lexema.length(), lexema, tokenType);
             }
-        }
+        }*/
 
-        return null;
-    }
-
-    @PostConstruct
-    private void launchRegEx() {
-        regEx.put(BlockComment, "(/\\*.*?\\*/).*");
-        regEx.put(LineComment, "(//(.*?)[\r$]?\n).*");
-        regEx.put(WhiteSpace, "( ).*");
-        regEx.put(OpenBrace, "(\\().*");
-        regEx.put(CloseBrace, "(\\)).*");
-        regEx.put(Semicolon, "(;).*");
-        regEx.put(Comma, "(,).*");
-        regEx.put(OpeningCurlyBrace, "(\\{).*");
-        regEx.put(ClosingCurlyBrace, "(\\}).*");
-        regEx.put(DoubleConstant, "\\b(\\d{1,9}\\.\\d{1,32})\\b.*");
-        regEx.put(IntConstant, "\\b(\\d{1,9})\\b.*");
-        regEx.put(Void, "\\b(void)\\b.*");
-        regEx.put(Int, "\\b(int)\\b.*");
-        regEx.put(Double, "\\b(int|double)\\b.*");
-        regEx.put(Tab, "(\\t).*");
-        regEx.put(NewLine, "(\\n).*");
-        regEx.put(Public, "\\b(public)\\b.*");
-        regEx.put(Private, "\\b(private)\\b.*");
-        regEx.put(False, "\\b(false)\\b.*");
-        regEx.put(True, "\\b(true)\\b.*");
-        regEx.put(Null, "\\b(null)\\b.*");
-        regEx.put(Return, "\\b(return)\\b.*");
-        regEx.put(New, "\\b(new)\\b.*");
-        regEx.put(Class, "\\b(class)\\b.*");
-        regEx.put(If, "\\b(if)\\b.*");
-        regEx.put(Else, "\\b(else)\\b.*");
-        regEx.put(While, "\\b(while)\\b.*");
-        regEx.put(Static, "\\b(static)\\b.*");
-        regEx.put(Point, "(\\.).*");
-        regEx.put(Plus, "(\\+{1}).*");
-        regEx.put(Minus, "(\\-{1}).*");
-        regEx.put(Multiply, "(\\*).*");
-        regEx.put(Divide, "(/).*");
-        regEx.put(EqualEqual, "(==).*");
-        regEx.put(Equal, "(=).*");
-        regEx.put(ExclamationEqual, "(\\!=).*");
-        regEx.put(Greater, "(>).*");
-        regEx.put(Less, "(<).*");
-        regEx.put(Identifier, "\\b([a-zA-Z]{1}[0-9a-zA-Z_]{0,31})\\b.*");
+        return of(TokenType.values()).stream()
+                .filter(tokenType -> tokenType.getMatcher(fromIndex, source).matches()).findFirst()
+                .map(tokenType -> new Token(fromIndex, fromIndex + tokenType.getMatcher(fromIndex, source).group(1).length(), tokenType.getMatcher(fromIndex, source).group(1), tokenType)).orElse(null);
     }
 }
